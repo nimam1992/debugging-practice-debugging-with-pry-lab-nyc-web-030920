4f178cd6b4a36811412ed338c5901f7024952604
Nima Moshtagh <nima.moshtagh1@gmail.com>
2020-02-18 03:28:26 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -2,8 +2,7 @@ require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
-  repeat = 10.times {'s'}
-  word = repeat + string
+  repeat = 10.times do {'s' + string}
   binding.pry
   word
   else
