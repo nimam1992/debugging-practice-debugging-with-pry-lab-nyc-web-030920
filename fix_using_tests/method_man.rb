d1bbb768e8a89862df5e3bb42111f35b483d3775
Nima Moshtagh <nima.moshtagh1@gmail.com>
2020-02-18 03:00:17 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -3,8 +3,8 @@ require 'pry'
 def start_game(player1, player2)
   "Hello #{player1} & #{playar2}"
 end
-binding.pry
 
 def play_game(player2)
   "#{player1} is better than #{player2}"
+  binding.pry
 end
